ce0d3074903ff1127f726a6520463b82449b0769
brunosanchez <bruno@oac.unc.edu.ar>
2018-05-29 01:24:14 -0300

    New things

--- feets_test.py
+++ feets_test.py
@@ -32,52 +32,52 @@ from astropy.io import fits
 data_path = os.path.abspath('ATLAS_LC/')
 
 def calc_features(varname):
-	obj_tab = fits.getdata(os.path.join(data_path, varname+'_objects_bos0109.fit'))
+    obj_tab = fits.getdata(os.path.join(data_path, varname+'_objects_bos0109.fit'))
 
-	det_tab = fits.getdata(os.path.join(data_path, varname+'_detection_bos0109.fit'))
+    det_tab = fits.getdata(os.path.join(data_path, varname+'_detection_bos0109.fit'))
 
-	fspace = feets.FeatureSpace(data=['time', 'magnitude', 'error'])
-					    #    'magnitude2', 'time2', 'error2'])
-	colnames = fspace.features_as_array_
-	features_o = np.zeros(colnames.shape)
-	features_c = np.zeros(colnames.shape)
-	#for irow in range(len(obj_tab[0:3])):
-	    #anobj = obj_tab[irow]
-	for anobj in obj_tab:
-	    objid = anobj['objid']
-	    detections = det_tab[det_tab['objid']==objid]
-	    o_tab = detections[detections['filter']=='o']
-	    c_tab = detections[detections['filter']=='c']
+    fspace = feets.FeatureSpace(data=['time', 'magnitude', 'error'])
+                        #    'magnitude2', 'time2', 'error2'])
+    colnames = fspace.features_as_array_
+    features_o = np.zeros(colnames.shape)
+    features_c = np.zeros(colnames.shape)
+    #for irow in range(len(obj_tab[0:3])):
+        #anobj = obj_tab[irow]
+    for anobj in obj_tab:
+        objid = anobj['objid']
+        detections = det_tab[det_tab['objid']==objid]
+        o_tab = detections[detections['filter']=='o']
+        c_tab = detections[detections['filter']=='c']
 
-	    #=========================================================================
-	    otime, omag, oerror = feets.preprocess.remove_noise(
-				      o_tab['mjd'], o_tab['m'], o_tab['dm'])
-	    #print 'total cmag points', len(cmag)
-	    if len(otime)>=20:
-		    lc = [otime, omag, oerror]
-		    try:
-			feat, val = fspace.extract(*lc)
-		    except:
-			import ipdb; ipdb.set_trace()
+        #=========================================================================
+        otime, omag, oerror = feets.preprocess.remove_noise(
+                      o_tab['mjd'], o_tab['m'], o_tab['dm'])
+        #print 'total cmag points', len(cmag)
+        if len(otime)>=20:
+            lc = [otime, omag, oerror]
+            try:
+                feat, val = fspace.extract(*lc)
+            except:
+                import ipdb; ipdb.set_trace()
 
-		    features_o = np.vstack([features_o, val])
+            features_o = np.vstack([features_o, val])
 
-	    #=========================================================================
-	    ctime, cmag, cerror = feets.preprocess.remove_noise(
-				      c_tab['mjd'], c_tab['m'], c_tab['dm'])
-	    #print 'total omag points', len(omag)
+        #=========================================================================
+        ctime, cmag, cerror = feets.preprocess.remove_noise(
+                      c_tab['mjd'], c_tab['m'], c_tab['dm'])
+        #print 'total omag points', len(omag)
             if len(ctime)>=20:
-		    lc = [ctime, cmag, cerror]
-		    feat, val = fspace.extract(*lc)
+                lc = [ctime, cmag, cerror]
+                feat, val = fspace.extract(*lc)
 
-		    features_c = np.vstack([features_c, val])
+                features_c = np.vstack([features_c, val])
 
 
-	features_o = np.delete(features_o, 0, 0)
-	features_c = np.delete(features_c, 0, 0)
+    features_o = np.delete(features_o, 0, 0)
+    features_c = np.delete(features_c, 0, 0)
 
-	np.savetxt(os.path.join(data_path,'features_o_'+varname+'.txt.gz'), features_o)
-	np.savetxt(os.path.join(data_path,'features_c_'+varname+'.txt.gz'), features_c)
+    np.savetxt(os.path.join(data_path,'features_o_'+varname+'.txt.gz'), features_o)
+    np.savetxt(os.path.join(data_path,'features_c_'+varname+'.txt.gz'), features_c)
 
 
 if __name__=='__main__':
---
+++ ml_experiment.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  ml_experiment.py
+#
+#  Copyright 2018 Bruno S <bruno@oac.unc.edu.ar>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+import numpy as np
+from sklearn import metrics
+from sklearn.model_selection import StratifiedKFold
+
+
+def experiment(clf, x, y, nfolds=10, printing=False):
+    skf = StratifiedKFold(n_splits=nfolds)
+    probabilities = None # np.array([])
+    predictions = np.array([])
+    y_testing = np.array([])
+
+    for train, test in skf.split(x, y):
+
+        x_train = x[train]
+        y_train = y[train]
+        clf.fit(x_train, y_train)
+
+        x_test = x[test]
+        y_test = y[test]
+        pr = clf.predict(x_test)
+        probs = clf.predict_proba(x_test)  #[:, 0]
+
+        probabilities = (
+            probs if probabilities is None else
+            np.vstack([probabilities, probs]))
+        predictions = np.hstack([predictions, pr])
+        y_testing = np.hstack([y_testing, y_test])
+
+    if printing:
+        print metrics.classification_report(y_testing, predictions)
+    fpr, tpr, thresholds = metrics.roc_curve(y_testing, 1.-probabilities[:, 0])
+    prec_rec_curve = metrics.precision_recall_curve(y_testing, 1.- probabilities[:, 0])
+    roc_auc = metrics.auc(fpr, tpr)
+    return {'fpr': fpr,
+            'tpr': tpr,
+            'thresh': thresholds,
+            'roc_auc': roc_auc,
+            'prec_rec_curve': prec_rec_curve,
+            'y_test': y_testing,
+            'predictions': predictions,
+            'probabilities': probabilities,
+            'confusion_matrix': metrics.confusion_matrix(y_testing, predictions),
+            'model': clf
+            }
